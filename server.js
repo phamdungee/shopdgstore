ac1ae356f3f615ad9c70bfcaadb67785df91379a
phamdungee <phamdungee@users.noreply.github.com>
2026-06-26 14:26:47 +0700

    Disable default index.html in express.static to let root route serve home.html

--- server.js
+++ server.js
@@ -66,6 +66,7 @@ app.use(cors({
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname), {
+  index: false,
   extensions: ['html']
 }));
 
